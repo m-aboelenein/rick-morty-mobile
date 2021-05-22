6c54cefe71994ac7fb1701ba7f981096ddb0e3fc
Mahmoud Aboelenein <aboelenein@pravica.io>
2021-05-22 13:55:51 +0200

    #0 - edit characters list item type

--- src/hooks/useCharacters/types.ts
+++ src/hooks/useCharacters/types.ts
@@ -23,14 +23,5 @@ export interface CharactersQueryVars {
 export interface Character {
   id: number;
   name: string;
-  status: string;
-  species: string;
-  type: string;
-  gender: string;
-  origin: string;
-  location: string;
   image: string;
-  episode: string;
-  url: string;
-  created: string;
 }
