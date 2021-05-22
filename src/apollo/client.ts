13131fcd61ab0047b4c75be462eefa1d4654d642
Mahmoud Aboelenein <aboelenein@pravica.io>
2021-05-22 03:45:35 +0200

    #0 - fix load more not updating

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,10 +1,36 @@
 import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {Character, CharactersData} from '../hooks/useCharacters/types';
 
 const BASE_URL = 'https://rickandmortyapi.com/graphql';
 
+const cache: InMemoryCache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        characters: {
+          keyArgs: ['filter'],
+          merge(existing: CharactersData, incoming: CharactersData) {
+            let results: Character[] = [];
+            if (existing && existing.results.length > 0) {
+              results = results.concat(existing.results);
+            }
+            if (incoming && incoming.results.length > 0) {
+              results = results.concat(incoming.results);
+            }
+            return {
+              ...incoming,
+              results,
+            };
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: BASE_URL,
-  cache: new InMemoryCache(),
+  cache,
 });
 
 export default client;
