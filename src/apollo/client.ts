82898f978ed4db0351a274d583c179dd9d0839af
Mahmoud Aboelenein <aboelenein@pravica.io>
2021-05-23 16:49:41 +0200

    #0 - re-structure caching behavior

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -9,26 +9,28 @@ const LINK = createHttpLink({
   fetch,
 });
 
+const charactersPaginationHandler = () => ({
+  keyArgs: ['filter'],
+  merge(existing: CharactersData, incoming: CharactersData) {
+    let results: Character[] = [];
+    if (existing && existing.results?.length > 0) {
+      results = results.concat(existing.results);
+    }
+    if (incoming && incoming.results?.length > 0) {
+      results = results.concat(incoming.results);
+    }
+    return {
+      ...incoming,
+      results,
+    };
+  },
+});
+
 const cache: InMemoryCache = new InMemoryCache({
   typePolicies: {
     Query: {
       fields: {
-        characters: {
-          keyArgs: ['filter'],
-          merge(existing: CharactersData, incoming: CharactersData) {
-            let results: Character[] = [];
-            if (existing && existing.results.length > 0) {
-              results = results.concat(existing.results);
-            }
-            if (incoming && incoming.results.length > 0) {
-              results = results.concat(incoming.results);
-            }
-            return {
-              ...incoming,
-              results,
-            };
-          },
-        },
+        characters: charactersPaginationHandler(),
       },
     },
   },
