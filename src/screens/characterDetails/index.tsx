5208f771466e68affe863818e9c6ddae4723e973
Mahmoud Aboelenein <aboelenein@pravica.io>
2021-05-23 16:50:16 +0200

    #0 - added loading and error handlers for character details

--- src/screens/characterDetails/index.tsx
+++ src/screens/characterDetails/index.tsx
@@ -1,11 +1,16 @@
 import {RouteProp, useRoute} from '@react-navigation/core';
-import React, {useEffect} from 'react';
-import {ListRenderItem, SafeAreaView} from 'react-native';
+import React, {useCallback, useEffect} from 'react';
+import {
+  ActivityIndicator,
+  ListRenderItem,
+  SafeAreaView,
+  Text,
+} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
 import CharacterDetailsHeader from '../../components/characterDetailsHeader';
 import EpisodesListItem from '../../components/episodesListItem';
 import useCharacter from '../../hooks/useCharacter';
-import {Episode} from '../../hooks/useCharacter/types';
+import {Character, Episode} from '../../hooks/useCharacter/types';
 import characterDetailsStyles from './styles';
 import {CharacterDetailsNavigationParams} from './types';
 
@@ -27,16 +32,44 @@ const CharacterDetails: React.FC = () => {
   const renderEpisodeItem: ListRenderItem<Episode> = ({item}) => (
     <EpisodesListItem episode={item} />
   );
+
+  const renderCharacterSummary: React.FC = () => {
+    return data?.character ? (
+      <CharacterDetailsHeader character={data?.character} />
+    ) : null;
+  };
+
+  const renderEmptyList = useCallback(() => {
+    if (loading) {
+      return (
+        <SafeAreaView style={characterDetailsStyles.loadingContainer}>
+          <ActivityIndicator size="large" color="green" />
+        </SafeAreaView>
+      );
+    }
+    if (error) {
+      return (
+        <SafeAreaView style={characterDetailsStyles.errorContainer}>
+          <Text style={characterDetailsStyles.errorMessage}>Sorry no data</Text>
+        </SafeAreaView>
+      );
+    }
+    return (
+      <SafeAreaView style={characterDetailsStyles.screenContainer}>
+        <Text>Sorry no data</Text>
+      </SafeAreaView>
+    );
+  }, [loading, error]);
+
   return (
     <SafeAreaView style={characterDetailsStyles.screenContainer}>
       <FlatList
-        ListHeaderComponent={
-          data?.character && (
-            <CharacterDetailsHeader character={data?.character} />
-          )
-        }
+        ListHeaderComponent={renderCharacterSummary}
         data={data?.character.episode}
+        ListEmptyComponent={renderEmptyList}
         renderItem={renderEpisodeItem}
+        keyExtractor={(item: Character) => item.id.toString()}
+        contentContainerStyle={characterDetailsStyles.listContentStyles}
       />
     </SafeAreaView>
   );
--- src/screens/characterDetails/styles.ts
+++ src/screens/characterDetails/styles.ts
@@ -6,5 +6,24 @@ const characterDetailsStyles = StyleSheet.create<CharacterDetailsStyles>({
     flex: 1,
     backgroundColor: 'black',
   },
+  listContentStyles: {
+    flexGrow: 1,
+  },
+  errorContainer: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorMessage: {
+    color: 'white',
+    fontSize: 16,
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 export default characterDetailsStyles;
