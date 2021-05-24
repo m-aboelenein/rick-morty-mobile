9c51c3844173e4d0a90f4887aa4f1a2ccf9d8cf2
Mahmoud Aboelenein <aboelenein@pravica.io>
2021-05-24 14:58:22 +0200

    #0 - optimize caching function and its type

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,6 +1,11 @@
-import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client';
+import {
+  ApolloClient,
+  createHttpLink,
+  FieldPolicy,
+  InMemoryCache,
+} from '@apollo/client';
 import fetch from 'cross-fetch';
-import {Character, CharactersData} from '../hooks/useCharacters/types';
+import {CharactersData} from '../hooks/useCharacters/types';
 
 const BASE_URL = 'https://rickandmortyapi.com/graphql';
 
@@ -9,19 +14,14 @@ const LINK = createHttpLink({
   fetch,
 });
 
-const charactersPaginationHandler = () => ({
+const charactersPaginationHandler = (): FieldPolicy => ({
   keyArgs: ['filter'],
   merge(existing: CharactersData, incoming: CharactersData) {
-    let results: Character[] = [];
-    if (existing && existing.results?.length > 0) {
-      results = results.concat(existing.results);
-    }
-    if (incoming && incoming.results?.length > 0) {
-      results = results.concat(incoming.results);
-    }
     return {
       ...incoming,
-      results,
+      results: existing
+        ? [...existing.results, ...incoming.results]
+        : incoming.results,
     };
   },
 });
